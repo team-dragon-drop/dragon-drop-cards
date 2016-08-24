525fc71a616cd4d01809376c0b81ae7d790f1a07
Steve Occhipinti <dev@stevenocchipinti.com>
2016-08-24 17:16:48 +1000

    Use Redux properly!

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
 import React, { Component } from 'react';
-import Column from './components/column.js';
+import { connect } from 'react-redux';
+
+import Column from './components/Column.js';
+import { addColumn } from './actions/index.js';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      columns: [
-        {
-          name: "The Good",
-          cards: [
-            { name: "We shipped stuff" },
-            { name: "Lots of cake!" },
-            { name: "New team members" },
-            { name: "Game of Thrones session" },
-            { name: "We made lots of money" }
-          ]
-        },
-        {
-          name: "The Bad",
-          cards: [
-            { name: "Lots of bugs" },
-            { name: "Refactoring the whole app is hard" },
-            { name: "Finding time for pairing is hard with meetings, etc." },
-            { name: "Build keeps breaking" },
-            { name: "VC quality is not great" },
-            { name: "VC quality is not great" },
-            { name: "VC quality is not great" },
-            { name: "VC quality is not great" }
-          ]
-        },
-        {
-          name: "The Questions",
-          cards: [
-            { name: "What is next for the project?" },
-            { name: "How often shuold we do retros?" },
-            { name: "Are there going to be anymore team changes before EOFY?" }
-          ]
-        }
-      ]
-    };
-  }
-
   render() {
-    let columns = this.state.columns.map((column, i) => {
+    let columns = this.props.columns.map((column, i) => {
       return <Column key={i} data={column} />;
     });
 
@@ -54,10 +17,28 @@ class App extends Component {
         </header>
         <main>
           {columns}
+          <button onClick={this.props.onAddColumn}>Add column</button>
         </main>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    columns: state.columns
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onAddColumn: () => {
+      dispatch(addColumn("test"));
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/components/column.js
+++ src/components/column.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Column = (props) => {
-  let cards = props.data.cards.map((card, i) => {
+let Column = ({data}) => {
+  let cardItems = data.cards.map((card, i) => {
     return <li key={i}>{card.name}</li>;
   });
 
   return (
     <div className="column">
-      <h2>{props.data.name}</h2>
-      <ul>{cards}</ul>
+      <h2>{data.name}</h2>
+      <ul>{cardItems}</ul>
       <div className="add-card-placeholder">
         <a href="#">Add a card...</a>
       </div>
