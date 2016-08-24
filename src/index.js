023a6e8c39a1c6c1645500ee483707acffb09b9c
Steve Occhipinti <dev@stevenocchipinti.com>
2016-08-24 15:41:37 +1000

    Some basic Redux stuff
    Currently not hooked up to the UI, but dispatching actions does update
    the state in the store!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducer from './reducers/index.js';
+
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+let store = createStore(reducer);
+
+console.log("Initial state = ", store.getState());
+
+let unsubscribe = store.subscribe(() =>
+  console.log("Updated state = ", store.getState())
 );
+
+import { addColumn } from './actions/index.js';
+store.dispatch(addColumn('foo'));
+
+unsubscribe();
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
