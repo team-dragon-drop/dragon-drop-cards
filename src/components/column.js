1bb8ed19383ed03942877926b1292ed89a734ce9
Steve Occhipinti <dev@stevenocchipinti.com>
2017-05-17 16:18:15 +1000

    Make <Column> droppable
    I enabled the endDrag function so we can see if dropping the card in
    actually worked - and it did!

--- src/components/column.js
+++ src/components/column.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { DropTarget } from 'react-dnd';
+
 import Card from './Card';
+
+const cardTarget = {
+  drop(props) {
+    return { name: props.data.name };
+  },
+};
+
+function collect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    canDrop: monitor.canDrop()
+  };
+}
 
 class Column extends React.Component {
   render() {
@@ -7,7 +23,7 @@ class Column extends React.Component {
       return <Card key={i} name={card.name} />;
     });
 
-    return (
+    return this.props.connectDropTarget(
       <div className="column">
         <h2>{this.props.data.name}</h2>
         <ul>{cardItems}</ul>
@@ -19,4 +35,4 @@ class Column extends React.Component {
   }
 };
 
-export default Column;
+export default DropTarget("Card", cardTarget, collect)(Column);
