2679e0f0d7e2652285def993318f6a170d9dcec4
Steve Occhipinti <steve.occhipinti@rea-group.com>
2017-05-17 16:18:15 +1000

    Make <Column> droppable
    I enabled the endDrag function so we can see if dropping the card in
    actually worked - and it did!

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,16 @@ const cardSource = {
     return { name: props.name };
   },
 
-  // endDrag(props, monitor) {
-  //   const item = monitor.getItem();
-  //   const dropResult = monitor.getDropResult();
+  endDrag(props, monitor) {
+    const item = monitor.getItem();
+    const dropResult = monitor.getDropResult();
 
-  //   if (dropResult) {
-  //     window.alert( // eslint-disable-line no-alert
-  //       `You dropped ${item.name} into ${dropResult.name}!`,
-  //     );
-  //   }
-  // },
+    if (dropResult) {
+      window.alert(
+        `You dropped ${item.name} into ${dropResult.name}!`,
+      );
+    }
+  },
 };
 
 function collect(connect, monitor) {
