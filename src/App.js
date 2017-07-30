c9f62b72cc262d332826ddf2f0cb39863a3a64bf
Steve Occhipinti <steve.occhipinti@rea-group.com>
2017-07-30 22:58:00 +0800

    Add routing so boards show for /b/:boardId
    Not sure whether to use `/b/:boardId` or `/:boardId`.
    The former is better because it carves out a namespace that will never clash
    with other routes we may want in the future (such as /my-account or something)
    but the later looks nicer.
    
    This currently lets you use whatever board ID in the URL you want as it will be
    used in the Firebase ref and the key will be dynamically created upon use.
    Not sure if this is good or bad :/
    
    <App> previously had the logic for how a board works but that was fine because
    it didn't really have anything else to be responsible for, but now <App> is
    responsible for routing and the board logic is in its own component.
    
    The <Board> component should probably have more refactoring as it has a lot of
    responsibilities but it'll do as is for now.

--- src/App.js
+++ src/App.js
@@ -1,74 +1,19 @@
 import React, { Component } from 'react';
-import Column from './components/Column';
-import { database } from 'firebase';
-import HTML5Backend from 'react-dnd-html5-backend';
-import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-import { DragDropContext } from 'react-dnd';
+import Board from './components/Board';
+import Home from './components/Home';
+import { Route } from 'react-router-dom';
 
-class App extends Component {
-  componentDidMount(){
-    database().ref('/-KkOZOdWvt73GuEUCYum/columns').on('value', snapshot => {
-      this.setState({
-        columns: snapshot.val()
-      });
-    });
-  }
-
-  constructor() {
-    super();
-    this.state = {columns:[]};
-  }
-
-  handleAddCard(id){
-    const content = prompt('Add Card');
-    if(content){
-      const ref = database().ref(`/-KkOZOdWvt73GuEUCYum/columns/${id}/cards/`).push();
-      ref.set({
-        name: content
-      });
-    }
-  }
-
-  addColumn(){
-    const content = prompt('Add Column');
-    if(content){
-      const ref = database().ref(`/-KkOZOdWvt73GuEUCYum/columns`).push();
-      ref.set({
-        name: content
-      });
-    }
-  }
-
+export default class App extends Component {
   render() {
-    let columns = null;
-    const columnData = this.state.columns;
-    if(columnData)
-      columns = Object.keys(columnData).map(key => {
-        return <Column
-          key={key}
-          id={key}
-          cards={columnData[key].cards}
-          name={columnData[key].name}
-          addCard={()=>this.handleAddCard(key)}
-        />;
-    });
-
     return (
-        <div className="App">
-        <AppBar
-          title="Dragon Drop"
-          showMenuIconButton={false}
-          iconElementRight={
-            <FlatButton onTouchTap={()=>this.addColumn()} label="Add Column" />
-          }
-          />
-         <main>
-            {columns}
-          </main>
-        </div>
+      <main>
+        <Route path="/" exact={true} render={() => (
+          <Home />
+        )} />
+        <Route path="/b/:boardId" render={({ match }) => (
+          <Board boardId={match.params.boardId} />
+        )} />
+      </main>
     );
   }
 }
-
-export default DragDropContext(HTML5Backend)(App);
