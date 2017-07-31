94354868f64b16ae40a917e639c2ba07609aa477
Steve Occhipinti <steve.occhipinti@rea-group.com>
2017-07-31 23:57:49 +0800

    WIP: Move Firebase logic up to Board
    This design will make it easier to extract the Firebase logic into yet another
    location that can have the responsibility of communicating with the backend.
    
    Eventually, this will make it easier to swap Firebase out for an in house
    backend, something like a self-hosted couchDB or a node server with MongoDB and
    socketIO.
    
    There is also packages that will connect firebase to a redux store so the app
    can use redux as per normal, but more research is required!
    https://medium.com/@prescottprue/firebase-with-redux-82d04f8675b9

--- src/components/Card.js
+++ src/components/Card.js
@@ -36,33 +36,33 @@ function collect(connect, monitor) {
 }
 
 class Card extends React.Component {
-  editCard(columnId,id,content){
-    const newContent = prompt('Edit Card',content);
-    if(newContent){
-      const ref = database().ref(`/-KkOZOdWvt73GuEUCYum/columns/${columnId}/cards`);
-      ref.child(id).set({
-        name: newContent
-      });
-    }
-  }
-
-  removeCard(columnId,id){
-    const ok = confirm("You sure about this?");  // eslint-disable-line
-    if(ok){
-      const ref = database().ref(`/-KkOZOdWvt73GuEUCYum/columns/${columnId}/cards`);
-      ref.child(id).remove();
-    }
-  }
-
   render() {
     return this.props.connectDragSource(
-      <li key={this.props.key} id={this.props.id} className="card clearfix"  onDoubleClick={()=>this.editCard(this.props.parentId,this.props.id,this.props.name)}>
-        <span style={{float:"left"}}>{this.props.name}</span>
-        <span style={{float:"right"}} onClick={()=>this.removeCard(this.props.parentId,this.props.id)}>
-          <CloseIcon className="card__close" /></span>
+      <li
+        key={this.props.key}
+        id={this.props.id}
+        className="card clearfix"
+        onDoubleClick={() => {
+          this.props.editCard(
+            this.props.parentId,
+            this.props.id,
+            this.props.name
+          );
+        }}
+      >
+
+        <span style={{ float: "left" }}>
+          {this.props.name}
+        </span>
+
+        <span
+          style={{ float: "right" }}
+          onClick={() => this.props.removeCard(this.props.parentId, this.props.id)}
+        >
+          <CloseIcon className="card__close" />
+        </span>
       </li>
     );
   }
 }
-
 export default DragSource("Card", cardSource, collect)(Card);
