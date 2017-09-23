177d1a3ad694f918dd4d24692cfa625e99ddee07
Steve Occhipinti <steve.occhipinti@rea-group.com>
2017-09-23 14:47:04 +1000

    Add a hidden actions panel for each card
    This panel visibility is toggled when the card is tapped.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { DragSource } from 'react-dnd';
 import RemoveCardButton from './RemoveCardButton';
+import ThumbsUpIcon from 'material-ui/svg-icons/action/thumb-up';
+import ThumbsDownIcon from 'material-ui/svg-icons/action/thumb-down';
+import EditIcon from 'material-ui/svg-icons/image/edit';
 
 const cardSource = {
   beginDrag(props) {
@@ -26,28 +29,53 @@ function collect(connect, monitor) {
 }
 
 class Card extends React.Component {
+  state = {
+    expanded: false
+  };
+
   render() {
     return this.props.connectDragSource(
       <li
         key={this.props.key}
         id={this.props.id}
-        className="card clearfix"
-        onDoubleClick={() => {
-          this.props.editCard(
-            this.props.parentId,
-            this.props.id,
-            this.props.name
-          );
-        }}
+        className={`card clearfix ${this.state.expanded && "card--expanded"}`}
       >
 
-        <span>
+        <div
+          className="card__text"
+          onClick={() => {
+            this.setState({...this.state, expanded: !this.state.expanded})
+          }}
+        >
           {this.props.name}
-        </span>
+        </div>
 
-        <RemoveCardButton
-          removeCard={() => this.props.removeCard(this.props.parentId, this.props.id)}
-        />
+        <div
+          style={{ display: this.state.expanded ? "inline-flex" : "none" }}
+          className="card__actions"
+        >
+          <span
+            onClick={(e) => { console.log("UP vote") }}
+          ><ThumbsUpIcon /></span>
+
+          <span
+            onClick={(e) => { console.log("DOWN vote") }}
+          ><ThumbsDownIcon /></span>
+
+          <span
+            onClick={(e) => {
+              this.props.editCard(
+                this.props.parentId,
+                this.props.id,
+                this.props.name
+              );
+            }}
+          ><EditIcon /></span>
+
+          <RemoveCardButton
+            removeCard={() => this.props.removeCard(this.props.parentId, this.props.id)}
+          />
+        </div>
       </li>
     );
   }
