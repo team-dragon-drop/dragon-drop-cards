dcabc88e34d7aaac36e81dec763cfd84b88de46e
Steve Occhipinti <steve.occhipinti@rea-group.com>
2018-03-25 01:05:44 +1100

    Make the title link to the homepage
    This change is mostly prettier doing its thing but really the only change is:
    
    ```
    onTitleTouchTap={() => (window.location.href = '/')}
    ```
    
    Probably should use react-router for this but `window.location.href` is
    cross-browser compatible and we don't do server-side rendering or host the app
    under a path which could effect this.

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import TextField from 'material-ui/TextField';
-import MuiAppBar from "material-ui/AppBar";
-import LinearProgress from "material-ui/LinearProgress";
-import AddIcon from 'material-ui/svg-icons/content/add';
-import IconButton from 'material-ui/IconButton';
-import { KeyboardShortcuts, KeyboardShortcutInhibitor } from "./KeyboardShortcuts";
+import React, {Component} from 'react'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import TextField from 'material-ui/TextField'
+import MuiAppBar from 'material-ui/AppBar'
+import LinearProgress from 'material-ui/LinearProgress'
+import AddIcon from 'material-ui/svg-icons/content/add'
+import IconButton from 'material-ui/IconButton'
+import {KeyboardShortcuts, KeyboardShortcutInhibitor} from './KeyboardShortcuts'
 
 export default class AppBar extends Component {
   state = {
     open: false,
-    newColumnName: ""
-  };
+    newColumnName: '',
+  }
 
   handleOpen = () => {
     this.setState({open: true}, () => {
-      this.refs.newColumn.focus();
-    });
-  };
+      this.refs.newColumn.focus()
+    })
+  }
 
   handleClose = () => {
-    this.setState({open: false});
-  };
+    this.setState({open: false})
+  }
 
-  handleChange = (e) => {
-    this.setState({newColumnName: e.target.value});
-  };
+  handleChange = e => {
+    this.setState({newColumnName: e.target.value})
+  }
 
-  handleSubmit = (e) => {
-    this.props.onButtonTouchTap(this.state.newColumnName);
-    this.setState({newColumnName: "", open: false});
-    e.preventDefault();
+  handleSubmit = e => {
+    this.props.onButtonTouchTap(this.state.newColumnName)
+    this.setState({newColumnName: '', open: false})
+    e.preventDefault()
   }
 
   loadingIndicator() {
     if (this.props.loading) {
-      return <LinearProgress mode="indeterminate" />;
+      return <LinearProgress mode="indeterminate" />
     } else {
       // This keeps the height consistent instead of jumping by 4 pixels
-      return <div style={{ height: "4px" }} />;
+      return <div style={{height: '4px'}} />
     }
   }
 
@@ -55,23 +55,26 @@ export default class AppBar extends Component {
         primary={true}
         form="addColumnForm"
         type="submit"
-      />
-    ];
+      />,
+    ]
 
     return (
       <div className="app-bar">
         <MuiAppBar
           title="Dragon Drop"
-          titleStyle={{textAlign: "center"}}
+          titleStyle={{textAlign: 'center'}}
+          onTitleTouchTap={() => (window.location.href = '/')}
           showMenuIconButton={false}
           iconElementRight={
-            <IconButton><AddIcon
-              onTouchTap={() => this.handleOpen()}
-              label={this.props.buttonLabel}
-            /></IconButton>
+            <IconButton>
+              <AddIcon
+                onTouchTap={() => this.handleOpen()}
+                label={this.props.buttonLabel}
+              />
+            </IconButton>
           }
         />
-        { this.loadingIndicator() }
+        {this.loadingIndicator()}
         <Dialog
           title="Add A New Column"
           actions={actions}
@@ -80,14 +83,20 @@ export default class AppBar extends Component {
           onRequestClose={this.handleClose}
         >
           <form id="addColumnForm" onSubmit={this.handleSubmit}>
-            <TextField onChange={this.handleChange} name="newColumn" ref="newColumn" />
+            <TextField
+              onChange={this.handleChange}
+              name="newColumn"
+              ref="newColumn"
+            />
           </form>
           <KeyboardShortcutInhibitor />
         </Dialog>
-        <KeyboardShortcuts keys={{
-          67: () => this.handleOpen()   // c
-        }}/>
+        <KeyboardShortcuts
+          keys={{
+            67: () => this.handleOpen(), // c
+          }}
+        />
       </div>
-    );
+    )
   }
 }
