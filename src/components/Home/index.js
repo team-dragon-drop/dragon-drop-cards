2981fe74cd012039dff6efbc62d22937bcde70fe
Milly Rowett <milly.rowett@rea-group.com>
2018-03-28 16:39:41 +1100

    Modify tagline to be more kid-friendly

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,7 +27,7 @@ export default class Home extends Component {
     return (
       <div className="homepage-container">
         <h1 className="homepage-title">Dragon Drop Cards</h1>
-        <em> Come for the dragons, stay for the &mdash; actually don't stay </em>
+        <em className="homepage-description"> Come for the dragons, stay for the cards.</em>
         <Logo />
         <div className="create-board">
           <TextField
