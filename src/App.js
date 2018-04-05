9e726310c3229894a13b44f2a28806e4045da703
Steve Occhipinti <steve.occhipinti@rea-group.com>
2018-04-05 19:08:38 +1000

    Attempt to log pageviews to GA upon clientside nav
    When following a <Link> and letting react-router change pages, the GA code does
    not fire again because its already loaded in the page.
    This change does fire for every route change and is supposed to send a pageview
    to GA.
    
    The problem is even though the function is fired for every route change (as
    evident by the console.log lines), looking at the GA realtime dashboard does not
    reflect these pages views, only the full page load ones.
    
    So this is currently not working as intended... not sure why yet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react';
-import Board from './components/Board';
-import Home from './components/Home';
-import { Route } from 'react-router-dom';
+import React, {Component} from 'react'
+import Board from './components/Board'
+import Home from './components/Home'
+import {Route} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
       <main>
-        <Route path="/" exact={true} render={() => (
-          <Home />
-        )} />
+        <Route path="/" exact={true} render={() => <Home />} />
 
-        <Route path="/b/:boardId" render={({ match }) => (
-          <Board boardId={match.params.boardId} />
-        )} />
+        <Route
+          path="/b/:boardId"
+          render={({match}) => <Board boardId={match.params.boardId} />}
+        />
+
+        <Route
+          path="/"
+          render={({location}) => {
+            console.log('can haz ga?', typeof window.ga === 'function')
+            if (typeof window.ga === 'function') {
+              window.ga('set', 'page', location.pathname + location.search)
+              window.ga('send', 'pageview')
+              console.log(
+                'Supposedly sent a pageview',
+                location.pathname + location.search,
+              )
+            }
+            return null
+          }}
+        />
       </main>
-    );
+    )
   }
 }
